22f274fb18ccea926dddca1d835591f2c66951fa
kaanarslann <kaanarslan_hp@hotmail.com>
2026-06-15 18:25:24 +0300

    Update product_detail_screen
    Added product detail info

--- mini_catalog_app/lib/views/product_detail_screen.dart
+++ mini_catalog_app/lib/views/product_detail_screen.dart
@@ -17,16 +17,89 @@ class _ProductDetailScreenState extends State<ProductDetailScreen> {
     return Scaffold(
       appBar: AppBar(title: Text("Back")),
 
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(
-            widget.product.image ?? "",
-            height: 350,
-            width: double.infinity,
-            fit: BoxFit.cover,
-          )
-        ],
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Image.network(
+                widget.product.image ?? "",
+                height: 350,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+          
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.product.name ?? "",
+                      style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                    ),
+                
+                    Text(
+                      widget.product.tagline ?? "",
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontWeight: FontWeight.w400,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.blueGrey,
+                      ),
+                    ),
+          
+                    SizedBox(height: 12),
+          
+                    Text(
+                      "Description",
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+          
+                    SizedBox(height: 4),
+          
+                    Text(
+                      widget.product.description ?? "",
+                      style: TextStyle(fontSize: 16, fontWeight: FontWeight.normal),
+                    ),
+        
+                    SizedBox(height: 12),
+        
+                    Text(
+                      widget.product.price ?? "",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.blue.shade400,
+                      ),
+                    ),
+        
+                    SizedBox(height: 16),
+        
+                    ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.blueGrey,
+                        minimumSize: Size(double.infinity, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        )
+                      ),
+                      child: Text(
+                        "Add To Cart",
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
+                      )
+                      )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
       ),
 
     );
